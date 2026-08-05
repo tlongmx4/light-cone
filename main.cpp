89998699dd8f1e38518ccf13c4f691c61c703f87
Timothy Long <st7gg4wk7b@privaterelay.appleid.com>
2026-08-05 19:07:11 -0400

    add camera and window setup

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
-#include <iostream>
 #include "raylib.h"
-#include <cmath>
 
-struct Cone {
+struct Event {
     double x; // space
-    double y; // time
+    double t; // time
     double z; // space
 };
 
@@ -16,8 +14,21 @@ int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Light Cone");
     SetTargetFPS(60);
 
+    Camera3D camera = { 0 };
+    camera.position = { 20.0f, 15.0f, 20.0f };
+    camera.target = { 0.0f, 5.0f, 0.0f };
+    camera.up = { 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
     while (!WindowShouldClose()) {
         BeginDrawing();
+        ClearBackground(BLACK);
+        BeginMode3D(camera);
+
+        DrawGrid(20, 1.0f);
+        DrawCubeWires({0, 0, 0}, 720, 720, 720, DARKGRAY);
+        EndMode3D();
         EndDrawing();
 
     }
